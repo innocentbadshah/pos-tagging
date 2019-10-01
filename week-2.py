dd0021866fc39f39d17c7de29955a25b68f1ce01
Ashish Singh <ak1186680@gmail.com>
2019-10-02 01:49:50 +0530

    Improved time complexity of week-2.py

--- week-2.py
+++ week-2.py
@@ -1,25 +1,19 @@
 from os import walk
-
 fo = open("./files/outfreq.txt","w")
-def CountFrequency(my_list): 
-      
-    # Creating an empty dictionary  
-    freq = {} 
-    for items in my_list: 
-        freq[items] = my_list.count(items) 
-      
-    for key, value in freq.items(): 
-        fo.write("% s : % d\n"%(key, value)) 
-  
 f = []
 for (dirpath, dirnames, filenames) in walk("./files/Train-corups/processed_files/"):
     f.extend(filenames)
     break
+
 data = []
 for file_name in f:
     with open("./files/Train-corups/processed_files/"+file_name, 'r') as file:
         data += file.read().split("\n")
-        
 
-CountFrequency(data)
+wordfreq = {}
+for word in data:
+    if word not in wordfreq:
+        wordfreq[word] = 0 
+    wordfreq[word] += 1
 
+fo.write(str(wordfreq).replace(", ","\n")[1:-1])
