013d9c91d25f4a416a43dfbb0e43c7175996b3ef
Ashish Singh <ak1186680@gmail.com>
2019-10-02 01:49:50 +0530

    add a resolution for list copy bug

--- week3.py
+++ week3.py
@@ -1,16 +1,15 @@
+import copy
 def sec(val):
 	return int(val[1])
 
 l = [line.rstrip('\n') for line in open("./files/outfreq.txt",'r')]
-r = l
-for item in r:
-	item = item.replace("'","")
-	item = item.rsplit(":",1)
 	
-
 for i in range(0,len(l)):
 	l[i] = l[i].rsplit(":", 1)
 	l[i][1] = int(l[i][1])
+
+r = copy.deepcopy(l)
+
 
 for i in range(0,len(r)):
 	head,sep,tail = str(r[i][0]).partition("_")
@@ -26,7 +25,6 @@ for word in r:
 	if word[0] not in tagfreq:
 		tagfreq[word[0]] = word[1]
 	tagfreq[word[0]] += word[1]
-print(tagfreq) 
 
 
 with open("./files/top10_words.txt", 'w') as f:
